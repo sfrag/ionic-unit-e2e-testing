4d6604e9474da68ad4b1bdf0730c70e54130968b
sfo <sfragua83@gmail.com>
2019-01-15 02:57:51 +0100

    Fixing e2e angular error

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Page } from './app.po';
+import { browser, by, element } from 'protractor';
+
 
 describe('App', () => {
   let page: Page;
+  browser.waitForAngularEnabled(false);
 
   beforeEach(() => {
     page = new Page();
